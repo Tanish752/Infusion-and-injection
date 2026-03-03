ce42c493a55e112885cb11a14033e1499075e472
Tanish752 <121241982+Tanish752@users.noreply.github.com>
2026-03-03 13:56:21 +0530

    Fix condition for drug timing check in I&I.py

--- I&I.py
+++ I&I.py
@@ -91,7 +91,7 @@ for category, drug, start, end, dur in all_times:
         primary_done = True
 
     # --- Case 2: Same drug repeated within 30 minutes ---
-    elif previous_drug == drug and previous_end in drug[start] or previous_end in drug[stop] and (start - previous_end).total_seconds() / 60 < 30:
+    elif previous_drug == drug and previous_end in drug[start] or previous_end in drug[end] and (start - previous_end).total_seconds() / 60 < 30:
         if (end - previous_end).total_seconds() / 60 > 30 and dur > 30:
             codes.append("96366")  # continuation if overlap >30 min
         else:
